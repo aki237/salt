9a7a75af8b362919a5c1a66513e80b1bc7b26318
Akilan Elango <akilan1997@gmail.com>
2016-06-05 06:57:16 +0530

    Templates : salt's template package (right now it does nothing)

---
+++ templates/salt_templates.go
@@ -0,0 +1,14 @@
+package templates
+
+import (
+	"github.com/aki237/salt"
+	"html/template"
+)
+
+func PushTemplate(filename string, w salt.ResponseBuffer ,fillers interface{}) (error) {
+	t,err := template.ParseFiles(filename)
+	if err != nil {
+		return err
+	}
+	return t.Execute(w, fillers)
+}
